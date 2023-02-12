b55ae649d0b970fef0d2435005ef96aae7b3563b
Mehmet Usta <ustamehmet83@hotmail.com>
2023-02-12 21:07:57 +0300

    Cucumber project

---
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -0,0 +1,51 @@
+package com.cydeo.step_definitions;
+
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_StepDefinitions {
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        System.out.println("user enters librarian username");
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        System.out.println("user enters librarian password");
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        System.out.println("user should see the dashboard");
+    }
+    @When("user enters student username")
+    public void user_enters_student_username() {
+        System.out.println("user enters student username");
+    }
+    @When("user enters student password")
+    public void user_enters_student_password() {
+        System.out.println("user enters student password");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,77 @@
+package com.cydeo.utilities;
+
+/*
+In this class only general utility methods that are not related to some specific page.
+ */
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class BrowserUtils {
+    /*
+    This method will accept int(in seconds) and execute Thread.sleep for given duration
+     */
+    public static void sleep(int second){
+        second*=1000;
+        try {
+            Thread.sleep(second);
+        }catch (InterruptedException e){
+
+        }
+    }
+
+    /*
+    This method accepts 3 arguments.
+    Arg1:webdriver
+    Arg2:expectedInUrl: for verify if the url contains given String.
+    -If condition matches, will break loop.
+    Args3: expectedInTitle to be compared against actualTitle
+     */
+    public static void switchWindowAndVerify(String expectedInUrl,String expectedInTitle){
+
+/*
+    This method accepts a String "exceptedTitle" and Asserts if it is true
+     */
+
+        Set<String> allWindowsHandles=Driver.getDriver().getWindowHandles();
+
+        for (String each:allWindowsHandles) {
+            Driver.getDriver().switchTo().window(each);
+            System.out.println("Current Url = " + Driver.getDriver().getCurrentUrl());
+            if (Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+        }
+        String actualTitle=Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+    }
+
+
+    public static void verifyTitle(String expectedTitle){
+        Assert.assertEquals(Driver.getDriver().getTitle(),expectedTitle);
+    }
+
+    public static void waitForInvisibility(WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
