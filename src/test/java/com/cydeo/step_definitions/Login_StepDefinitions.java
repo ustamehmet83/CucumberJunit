ff96885b5dad8e9070db1273584016f73417c3b2
Mehmet Usta <ustamehmet83@hotmail.com>
2023-02-12 21:34:01 +0300

    day14 login.features

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -9,43 +10,35 @@ public class Login_StepDefinitions {
     public void user_enters_librarian_username() {
         System.out.println("user enters librarian username");
     }
+
     @When("user enters librarian password")
     public void user_enters_librarian_password() {
         System.out.println("user enters librarian password");
     }
+
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
         System.out.println("user should see the dashboard");
     }
+
     @When("user enters student username")
     public void user_enters_student_username() {
         System.out.println("user enters student username");
     }
+
     @When("user enters student password")
     public void user_enters_student_password() {
         System.out.println("user enters student password");
     }
 
 
+    @When("user enters admin username")
+    public void user_enters_admin_username() {
+        System.out.println("user enters admin username");
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @And("user enters admin password")
+    public void user_enters_admin_password() {
+        System.out.println("user enters admin password");
+    }
 }
