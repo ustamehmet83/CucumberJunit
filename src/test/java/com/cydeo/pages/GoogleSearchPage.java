483df3620185a3edb2f27604bd4f14261990c4db
Mehmet Usta <ustamehmet83@hotmail.com>
2023-02-19 17:29:11 +0300

    day15 login. Features

---
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -0,0 +1,30 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleSearchPage {
+
+
+    //create constructor
+    //initialize the driver instance and this class' instance using PageFactory.initElements method
+    public GoogleSearchPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    //We can start locating web elements using @Findby annotation
+
+
+    @FindBy(name="q")
+    public WebElement searchBox;
+
+
+
+
+
+
+
+
+}
--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,11 +1,19 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.text.Utilities;
 
 public class GoogleStepDefinitions {
+
+    GoogleSearchPage googleSearchPage=new GoogleSearchPage();
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
         Driver.getDriver().get("https://www.google.com");
@@ -20,10 +28,15 @@ public class GoogleStepDefinitions {
 
     @When("user types apple and clicks enter")
     public void user_types_apple_and_clicks_enter() {
+        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
 
     }
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
 
+        String expectedTitle="apple - Google'da Ara";
+        String actualTitle=Driver.getDriver().getTitle();
+//Junit assertion accepts first arg as expected, second arg as expected
+        Assert.assertEquals("Title is not as expected",expectedTitle,actualTitle);
     }
 }
