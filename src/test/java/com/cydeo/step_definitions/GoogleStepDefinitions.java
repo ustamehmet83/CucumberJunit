3fb2976a670c393c664cb9ad36e49b216fc5c8b2
Mehmet Usta <ustamehmet83@hotmail.com>
2023-02-19 19:54:27 +0300

    day16 GoogleStepDefinitions

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -13,40 +13,51 @@ import javax.swing.text.Utilities;
 
 public class GoogleStepDefinitions {
 
-    GoogleSearchPage googleSearchPage=new GoogleSearchPage();
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
         Driver.getDriver().get("https://www.google.com");
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
-        String expectedTitle="Google";
-        String actualTitle=Driver.getDriver().getTitle();
-        Assert.assertEquals(actualTitle,expectedTitle);
+        String expectedTitle = "Google";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
         Driver.closeDriver();
     }
 
-    @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
-        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
-
-    }
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
-
-        String expectedTitle="apple - Google'da Ara";
-        String actualTitle=Driver.getDriver().getTitle();
-//Junit assertion accepts first arg as expected, second arg as expected
-        Assert.assertEquals("Title is not as expected",expectedTitle,actualTitle);
-    }
 
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String string) {
-
+        googleSearchPage.searchBox.sendKeys(string + Keys.ENTER);
     }
-    @Then("user sees {string} in the google title")
-    public void user_sees_in_the_google_title(String string) {
 
+
+
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String searchKeyword) {
+
+        String expectedTitle = searchKeyword + " - Google'da Ara";
+        String actualTitle = Driver.getDriver().getTitle();
+        //Junit assertion accepts first arg as expected, second arg as expected
+        Assert.assertEquals("Title is not as expected", expectedTitle, actualTitle);
+    }
+
+
+    @When("user types apple and clicks enter")
+    public void user_types_and_clicks_enter2() {
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+    }
+
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+
+        String expectedTitle = "apple - Google'da Ara";
+        String actualTitle = Driver.getDriver().getTitle();
+        //Junit assertion accepts first arg as expected, second arg as expected
+        Assert.assertEquals("Title is not as expected", expectedTitle, actualTitle);
     }
 
 }
