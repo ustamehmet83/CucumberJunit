28a432f419e4e6360170cc85d8e023425668bdd5
Mehmet Usta <ustamehmet83@hotmail.com>
2023-02-21 17:09:20 +0300

    day17 Dropdown Table

---
+++ src/test/java/com/cydeo/pages/DropdownsPage.java
@@ -0,0 +1,12 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DropdownsPage {
+
+
+    public DropdownsPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
