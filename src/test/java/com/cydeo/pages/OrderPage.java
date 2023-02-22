6a9a606882485a614fa1ee00c392f8618f233945
Mehmet Usta <ustamehmet83@hotmail.com>
2023-02-22 14:04:38 +0300

    Order_StepDefinitions

---
+++ src/test/java/com/cydeo/pages/OrderPage.java
@@ -0,0 +1,43 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class OrderPage extends BasePage{
+
+    public OrderPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "//select[@name='product']")
+    public WebElement productDropdown;
+
+    @FindBy (xpath = "//input[@placeholder='Enter amount desired']")
+    public WebElement inputQuantity;
+    @FindBy (xpath = "//input[@placeholder='Full name of the customer']")
+    public WebElement inputCustomerName;
+    @FindBy (xpath = "//input[@placeholder='Street address of the customer']")
+    public WebElement inputStreet;
+    @FindBy (xpath = "//input[@placeholder='City where the customer lives']")
+    public WebElement inputCity;
+    @FindBy (xpath = "//input[@placeholder='State where the customer lives']")
+    public WebElement inputState;
+    @FindBy (xpath = "//input[@name='zip']")
+    public WebElement inputZipCode;
+    @FindBy (xpath = "//label[@class='radio']")
+    public List<WebElement> inputPaymentInfo;
+
+    @FindBy (xpath = "//input[@placeholder='The number on the card']")
+    public WebElement inputNumberOnCard;
+    @FindBy (xpath = "//input[@placeholder='e.g. 04/24']")
+    public WebElement inputExpiryDate;
+    @FindBy (xpath = "//button[.='Process Order']")
+    public WebElement inputProcessOrder;
+    @FindBy (xpath = "//tbody//th/following-sibling::td[1]")
+    public WebElement firstRowOfTable;
+
+}
--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -1,60 +1,106 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.BasePage;
+import com.cydeo.pages.OrderPage;
+import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Order_StepDefinitions {
+    WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
+    BasePage basePage = new BasePage();
+    OrderPage orderPage = new OrderPage();
+
     @Given("user is already logged in and on order page")
     public void user_is_already_logged_in_and_on_order_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("web.table.url"));
+        webTableLoginPage.login();
+        basePage.orders.click();
 
     }
+
     @When("user selects product type {string}")
     public void user_selects_product_type(String string) {
 
-    }
-    @When("user enters quantity {string}")
-    public void user_enters_quantity(String string) {
+        Select select = new Select(orderPage.productDropdown);
+        select.selectByVisibleText(string);
 
     }
+
+    @When("user enters quantity {int}")
+    public void user_enters_quantity(int arg0) {
+        //accepting int argument and sending it using sendKeys() method
+        //since sendKeys() method only accepts String, we need to either concat with ""
+        //or send String.valueOf(int)
+        //clear() method will delete whatever is in input box
+        //orderPage.inputQuantity.clear();
+
+        //imitating pressing back_space button to delete existing input
+        orderPage.inputQuantity.sendKeys(Keys.BACK_SPACE);
+        orderPage.inputQuantity.sendKeys(String.valueOf(arg0));
+
+    }
+
     @When("user enters customer name {string}")
     public void user_enters_customer_name(String string) {
-
+        orderPage.inputCustomerName.sendKeys(string);
     }
+
     @When("user enters street {string}")
     public void user_enters_street(String string) {
-
+        orderPage.inputStreet.sendKeys(string);
     }
+
     @When("user enters city {string}")
     public void user_enters_city(String string) {
-
+        orderPage.inputCity.sendKeys(string);
     }
+
     @When("user enter state {string}")
     public void user_enter_state(String string) {
-
+        orderPage.inputState.sendKeys(string);
     }
+
     @When("user enters zipcode {string}")
     public void user_enters_zipcode(String string) {
-
+        orderPage.inputZipCode.sendKeys(string);
     }
+
     @When("user selects credit card type {string}")
-    public void user_selects_credit_card_type(String string) {
-
+    public void user_selects_credit_card_type(String string) throws InterruptedException {
+        for (WebElement webElement : orderPage.inputPaymentInfo) {
+            if (webElement.getText().equals(string)) {
+                webElement.click();
+            }
+        }
     }
+
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-
+        orderPage.inputNumberOnCard.sendKeys(string);
     }
+
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
-
+        orderPage.inputExpiryDate.sendKeys(string);
     }
+
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-
+        orderPage.inputProcessOrder.click();
     }
+
     @Then("user should see {string} in first row of the web table")
     public void user_should_see_in_first_row_of_the_web_table(String string) {
-
+        String actualFirstRowOfTable = orderPage.firstRowOfTable.getText();
+        Assert.assertTrue(actualFirstRowOfTable.equals(string));
     }
 }
